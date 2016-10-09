553b607b9659ae2bde07395762dcb856c2489841
a-lysenko <aleksey.lysenko@gmail.com>
2016-10-09 16:57:05 +0300

    - added .idea to gitignore - added mq with tutorial code

---
+++ app/mq.js
@@ -0,0 +1,33 @@
+var q = 'tasks';
+var URL = 'amqp://gootiafl:s8rlkMIBgMRQ_IHLi87ZgWXNwa2TFtuP@spotted-monkey.rmq.cloudamqp.com/gootiafl';
+
+var open = require('amqplib').connect(URL);
+
+// Publisher 
+open
+    .then(function (conn) {
+        return conn.createChannel();
+    })
+    .then(function (ch) {
+        return ch.assertQueue(q).then(function (ok) {
+            return ch.sendToQueue(q, new Buffer('something to do'));
+        });
+    })
+    .catch(console.warn);
+
+// Consumer 
+open
+    .then(function (conn) {
+        return conn.createChannel();
+    })
+    .then(function (ch) {
+        return ch.assertQueue(q).then(function (ok) {
+            return ch.consume(q, function (msg) {
+                if (msg !== null) {
+                    console.log(msg.content.toString());
+                    ch.ack(msg);
+                }
+            });
+        });
+    })
+    .catch(console.warn);
