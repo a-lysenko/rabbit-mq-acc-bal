78871841b43f981992b47f76cd55cc10fe8c242e
a-lysenko <lysa@ciklum.com>
2016-10-11 17:59:11 +0300

    - added url to mLab db in configuration - created file for schemas

--- app/configuration.js
+++ app/configuration.js
@@ -1,5 +1,7 @@
-const URL = 'amqp://gootiafl:s8rlkMIBgMRQ_IHLi87ZgWXNwa2TFtuP@spotted-monkey.rmq.cloudamqp.com/gootiafl';
+const amqpURL = 'amqp://gootiafl:s8rlkMIBgMRQ_IHLi87ZgWXNwa2TFtuP@spotted-monkey.rmq.cloudamqp.com/gootiafl';
 const amqpQueue = 'amqpQueue-default';
+const mLabURL = 'mongodb://accountbalance:Password1@ds021356.mlab.com:21356/accountbalance';
 
-exports.amqpURL = URL;
+exports.amqpURL = amqpURL;
 exports.amqpQueue = amqpQueue;
+exports.mLabURL = mLabURL;
