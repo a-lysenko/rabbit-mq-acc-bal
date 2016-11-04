dd6624933de921c0f2304b01872a02fc0b0b744b
a-lysenko <lysa@ciklum.com>
2016-11-04 16:44:08 +0200

    - updated mLab URL in config

--- app/configuration.js
+++ app/configuration.js
@@ -1,5 +1,5 @@
 const amqpURL = 'amqp://gootiafl:s8rlkMIBgMRQ_IHLi87ZgWXNwa2TFtuP@spotted-monkey.rmq.cloudamqp.com/gootiafl';
-const mLabURL = 'mongodb://accountbalance:Password1@ds021356.mlab.com:21356/accountbalance';
+const mLabURL = 'mongodb://lysenko:Password1@ds021356.mlab.com:21356/accountbalance';
 const amqpQueueRequest = 'amqpQueue-request';
 const amqpQueueResponse = 'amqpQueue-response';
 
