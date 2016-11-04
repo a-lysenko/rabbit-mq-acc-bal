5b8fc5eac229b4dfd20c75a61cc6c82095444db9
a-lysenko <lysa@ciklum.com>
2016-11-04 17:09:09 +0200

    - prepared multiple routes for application in a whole

--- app/routes.js
+++ app/routes.js
@@ -1,11 +1,55 @@
 module.exports = (app, mq) => {
     app.get('/', (req, res) => {
-
-
         res.send('hello world');
     });
 
-    app.get('/new-one', (req, res) => {
+    app.get('/get_hotel/:id', (req, res) => {
+        const currentDate = Date.now();
+        mq.requestQueue.publish('Data from new one on ' + currentDate)
+            .then((bufferIsAllowed) => {
+                if (!bufferIsAllowed) {
+                    console.log('Error! Queue buffer is full!');
+
+                    res.status(503).send('Queue buffer is full!');
+                }
+            });
+
+        const handlerId = mq.responseQueue.subscribe((msg) => {
+            //console.log('Gotten msg', msg, 'type', typeof msg);
+            //console.log('countResSent', countResSent);
+            res.status(200).json({
+                msg: msg.content.toString()
+            });
+
+            console.log('handlerId', handlerId);
+            mq.responseQueue.unsubscribe(handlerId);
+        })
+    });
+
+    app.get('/get_hotels', (req, res) => {
+        const currentDate = Date.now();
+        mq.requestQueue.publish('Data from new one on ' + currentDate)
+            .then((bufferIsAllowed) => {
+                if (!bufferIsAllowed) {
+                    console.log('Error! Queue buffer is full!');
+
+                    res.status(503).send('Queue buffer is full!');
+                }
+            });
+
+        const handlerId = mq.responseQueue.subscribe((msg) => {
+            //console.log('Gotten msg', msg, 'type', typeof msg);
+            //console.log('countResSent', countResSent);
+            res.status(200).json({
+                msg: msg.content.toString()
+            });
+
+            console.log('handlerId', handlerId);
+            mq.responseQueue.unsubscribe(handlerId);
+        })
+    });
+
+    app.post('/add_hotel', (req, res) => {
         const currentDate = Date.now();
         mq.requestQueue.publish('Data from new one on ' + currentDate)
             .then((bufferIsAllowed) => {
