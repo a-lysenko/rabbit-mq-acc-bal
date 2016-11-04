66ab2aae51017f1b071c4ca59a0632dd93bd40e0
a-lysenko <lysa@ciklum.com>
2016-11-04 16:45:12 +0200

    - renamed file schemas -> model - implemented mongo schema for hotel

---
+++ app/database/model.js
@@ -0,0 +1,26 @@
+function createModel(mongoose) {
+    const hotelSchema = mongoose.Schema({
+        name: {
+            type: String,
+            minlength: 3,
+            maxlength: 50
+        },
+        description: {
+            type: String,
+            maxlength: 250
+        },
+        starLevel: {
+            type: Number,
+            min: 1,
+            max: 5
+        }
+    });
+
+    const Hotel = mongoose.model('Hotel', hotelSchema);
+
+    return {
+        Hotel
+    }
+}
+
+module.exports = createModel;
