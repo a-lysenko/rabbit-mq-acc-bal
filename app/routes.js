c86a90987d5cee35321fc0ae1ce1757b233d74e0
a-lysenko <lysa@ciklum.com>
2016-11-09 16:15:18 +0200

    - updated mock data - provided mock data in response for single hotel

--- app/routes.js
+++ app/routes.js
@@ -1,29 +1,44 @@
 module.exports = function (app, mq) {
+    // TODO remove mock after related implementation
+    const mockHotels = [
+        {
+            id: Math.random().toString().slice(-6),
+            name: 'Hotel 1',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci ' +
+            'cupiditate totam quidem? Cumque praesentium incidunt cum quis reprehenderit ' +
+            'aspernatur velit aut recusandae alias similique aliquid, nulla accusamus officiis ' +
+            'repellat explicabo porro ex! Similique, dolorum! Tempore dolorem, nihil eveniet, debitis ' +
+            'dolorum, animi deserunt alias, nobis incidunt libero quae cupiditate similique optio!',
+            rate: 3
+        },
+        {
+            id: Math.random().toString().slice(-6),
+            name: 'Hotel 2',
+            description: 'Lorem',
+            rate: 3
+        },
+        {
+            id: Math.random().toString().slice(-6),
+            name: 'Hotel 1-2-3',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci ' +
+            'cupiditate totam quidem? Cumque praesentium incidunt cum quis reprehenderit ' +
+            'aspernatur velit aut recusandae alias similique aliquid, nulla accusamus officiis ' +
+            'repellat explicabo porro ex! Similique, dolorum! Tempore dolorem, nihil eveniet, debitis ' +
+            'dolorum, animi deserunt alias, nobis incidunt libero quae cupiditate similique optio!',
+            rate: 5
+        }
+    ];
+
     app.get('/', (req, res) => {
         res.send('hello world');
     });
 
     app.get('/get_hotel/:id', (req, res) => {
-        const currentDate = Date.now();
-        mq.requestQueue.publish('Data from new one on ' + currentDate)
-            .then((bufferIsAllowed) => {
-                if (!bufferIsAllowed) {
-                    console.log('Error! Queue buffer is full!');
-
-                    res.status(503).send('Queue buffer is full!');
-                }
-            });
-
-        const handlerId = mq.responseQueue.subscribe((msg) => {
-            //console.log('Gotten msg', msg, 'type', typeof msg);
-            //console.log('countResSent', countResSent);
-            res.status(200).json({
-                msg: msg.content.toString()
-            });
-
-            console.log('handlerId', handlerId);
-            mq.responseQueue.unsubscribe(handlerId);
-        })
+        // TODO replace a mock with real implementation
+        const mockHotel = mockHotels.find((hotel) => {
+                return hotel.id === req.params.id;
+            }) || {};
+        res.status(200).json(mockHotel);
     });
 
     app.get('/get_hotels', (req, res) => {
@@ -49,34 +64,7 @@ module.exports = function (app, mq) {
         //     mq.responseQueue.unsubscribe(handlerId);
         // })
 
-        res.status(200).json([
-            {
-                id: 'id_01',
-                name: 'Hotel 1',
-                description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci ' +
-                'cupiditate totam quidem? Cumque praesentium incidunt cum quis reprehenderit ' +
-                'aspernatur velit aut recusandae alias similique aliquid, nulla accusamus officiis ' +
-                'repellat explicabo porro ex! Similique, dolorum! Tempore dolorem, nihil eveniet, debitis ' +
-                'dolorum, animi deserunt alias, nobis incidunt libero quae cupiditate similique optio!',
-                rate: 3
-            },
-            {
-                id: 'id_02',
-                name: 'Hotel 2',
-                description: 'Lorem',
-                rate: 3
-            },
-            {
-                id: 'id_03',
-                name: 'Hotel 1-2-3',
-                description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci ' +
-                'cupiditate totam quidem? Cumque praesentium incidunt cum quis reprehenderit ' +
-                'aspernatur velit aut recusandae alias similique aliquid, nulla accusamus officiis ' +
-                'repellat explicabo porro ex! Similique, dolorum! Tempore dolorem, nihil eveniet, debitis ' +
-                'dolorum, animi deserunt alias, nobis incidunt libero quae cupiditate similique optio!',
-                rate: 5
-            }
-        ]);
+        res.status(200).json(mockHotels);
     });
 
     app.post('/add_hotel', (req, res) => {
