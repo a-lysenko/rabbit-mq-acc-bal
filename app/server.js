fe7b575ba5ee8a3f10991f9bccaea964418b6d2a
a-lysenko <aleksey.lysenko@gmail.com>
2016-11-12 03:14:56 +0200

    - added body-parser as npm dependency - used body-parser in express setup

--- app/server.js
+++ app/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
 // set our port
 const port = process.env.PORT || 3000;
@@ -7,6 +8,12 @@ const configuration = require('./configuration');
 
 // set the static files location /public/img will be /img for users
 app.use(express.static(__dirname + '/../public'));
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
 
 const mq = require('./mq/mq')(configuration);
 
