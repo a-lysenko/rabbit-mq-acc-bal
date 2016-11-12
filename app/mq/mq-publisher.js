17cc0112673721cbe59d3c43fbd5d2e34ba24d4b
a-lysenko <aleksey.lysenko@gmail.com>
2016-11-12 03:18:40 +0200

    - added logging in mq publisher process - changed logic to work with message from point of its type

--- app/mq/mq-publisher.js
+++ app/mq/mq-publisher.js
@@ -1,20 +1,21 @@
-(function () {
-    module.exports = (createdChannel, amqpQueue) => {
-        let channelInstance;
+module.exports = (createdChannel, amqpQueue) => {
+    let channelInstance;
 
-        function publish(message) {
-            return createdChannel
-                .then((channel) => {
-                    channelInstance = channel;
-                    return channelInstance.assertQueue(amqpQueue);
-                })
-                .then((ok) => {
-                    console.info('MQ Event. Asserting on publishing in', amqpQueue, 'queue. ok', ok);
+    function publish(message) {
+        return createdChannel
+            .then((channel) => {
+                channelInstance = channel;
+                return channelInstance.assertQueue(amqpQueue);
+            })
+            .then((ok) => {
+                console.info('MQ Event. Asserting on publishing in', amqpQueue, 'queue. ok', ok);
+                console.info('\t with message:', message);
 
-                    return channelInstance.sendToQueue(amqpQueue, Buffer.from(message));
-                });
-        }
+                const stringifiedMsg = JSON.stringify(message);
 
-        return publish;
-    };
-})();
+                return channelInstance.sendToQueue(amqpQueue, new Buffer(stringifiedMsg));
+            });
+    }
+
+    return publish;
+};
