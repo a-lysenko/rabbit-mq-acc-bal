051e30913dd0bc3fb4420e569ae6128e871dbd86
a-lysenko <aleksey.lysenko@gmail.com>
2016-11-15 00:41:32 +0200

    - updated handling of 'get_hotels' - fixed bug with type converting from buffer-type content received from queue

--- app/routes.js
+++ app/routes.js
@@ -42,29 +42,35 @@ module.exports = function (app, mq) {
     });
 
     app.get('/get_hotels', (req, res) => {
-        // TODO - uncomment this and remove a mock below
-        // const currentDate = Date.now();
-        // mq.requestQueue.publish('Data from new one on ' + currentDate)
-        //     .then((bufferIsAllowed) => {
-        //         if (!bufferIsAllowed) {
-        //             console.log('Error! Queue buffer is full!');
-        //
-        //             res.status(503).send('Queue buffer is full!');
-        //         }
-        //     });
-        //
-        // const handlerId = mq.responseQueue.subscribe((msg) => {
-        //     //console.log('Gotten msg', msg, 'type', typeof msg);
-        //     //console.log('countResSent', countResSent);
-        //     res.status(200).json({
-        //         msg: msg.content.toString()
-        //     });
-        //
-        //     console.log('handlerId', handlerId);
-        //     mq.responseQueue.unsubscribe(handlerId);
-        // })
+        // TODO - this endpoint uses the same queue as endpoint 'add_hotel' what can lead to an error
+        // so queues should be splitted. Probably by some identifier in scope of the same name although
+        let {name, description, rate} = req.query;
 
-        res.status(200).json(mockHotels);
+        mq.requestQueue.publish({
+            action: 'get',
+            data: {
+                name,
+                description,
+                rate
+            }
+        })
+            .then((bufferIsAllowed) => {
+                if (!bufferIsAllowed) {
+                    console.log('Routes. Error! Queue buffer is full on getting hotels!');
+
+                    res.status(503).send('Queue buffer is full!');
+                }
+            });
+
+        const handlerId = mq.responseQueue.subscribe((msg) => {
+            console.info('Response on route "get_hotels". Called to response with message props:');
+            console.info('\t msg.fields:', msg.fields);
+            console.info('\t msg.content:', msg.content);
+            res.status(200).json(JSON.parse(msg.content));
+
+            console.log('handlerId', handlerId);
+            mq.responseQueue.unsubscribe(handlerId);
+        });
     });
 
     app.post('/add_hotel', (req, res) => {
@@ -82,10 +88,10 @@ module.exports = function (app, mq) {
             });
 
         const handlerId = mq.responseQueue.subscribe((msg) => {
-            console.info('Response on route "add_hotel". Called to response with message', msg);
-            res.status(200).json({
-                msg: msg.content.toString()
-            });
+            console.info('Response on route "add_hotel". Called to response with message props:');
+            console.info('\t msg.fields:', msg.fields);
+            console.info('\t msg.content:', msg.content);
+            res.status(200).json(JSON.parse(msg.content));
 
             console.log('handlerId', handlerId);
             mq.responseQueue.unsubscribe(handlerId);
